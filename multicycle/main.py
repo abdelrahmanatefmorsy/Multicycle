3817568209fdd0f3f3201dc6aaa3e5278879af2b
abdelrahman atef morsy <abdelrahman.atef.morsy@gmail.com>
2024-12-27 13:23:47 +0200

    add 2buttons

--- multicycle/main.py
+++ multicycle/main.py
@@ -19,11 +19,15 @@ def add_entry_and_button():
     var = tk.StringVar()
     sub_frame = tk.Frame(dynamic_frame, bg="#f0f0f0")
     sub_frame.pack(fill="x", pady=2)
+    
     entry = tk.Entry(sub_frame, textvariable=var, justify="right", width=20, font=("Arial", 12))
     entry.pack(side="right", padx=3, pady=3)
-    button = tk.Button(sub_frame, text="Cycle", command=lambda: check_code(var.get()), bg="#4CAF50", fg="white", relief="solid", padx=10, pady=5)
-    button.pack(side="right", padx=3)
-
+    
+    cycle_button = tk.Button(sub_frame, text="Cycle", command=lambda: check_code(var.get()), bg="#4CAF50", fg="white", relief="solid", padx=10, pady=5)
+    cycle_button.pack(side="right", padx=3)
+    
+    delete_button = tk.Button(sub_frame, text="Delete", command=sub_frame.destroy, bg="#FF0000", fg="white", relief="solid", padx=10, pady=5)
+    delete_button.pack(side="right", padx=3)
 # table of registers
 main_frame = tk.Frame(root, bg="#f0f0f0")
 main_frame.pack(pady=10, padx=10, fill="both", expand=True)
@@ -37,14 +41,15 @@ tk.Label(table_frame, text="Value", bg="#f0f0f0", fg="black", font=("Arial", 10,
 for i, reg in enumerate(Registers):
     tk.Label(table_frame, text=reg, bg="#f0f0f0", fg="black", font=("Arial", 10), anchor="w").grid(row=i + 1, column=0, padx=5, pady=2)
     globalVar.register_vars[reg] = tk.StringVar(value="0")
-    tk.Entry(table_frame, textvariable=globalVar.register_vars[reg], width=10).grid(row=i + 1, column=1, padx=5, pady=2)
+    tk.Entry(table_frame, textvariable=globalVar.register_vars[reg], width=10,font=12).grid(row=i + 1, column=1, padx=5, pady=2)
 
 dynamic_frame = tk.Frame(main_frame, bg="#f0f0f0")
 dynamic_frame.pack(side="right", padx=10, pady=10, fill="both", expand=True)
-
-
-add_button = tk.Button(root, text="Add", command=add_entry_and_button, bg="#008CBA", fg="white", relief="solid", padx=10, pady=5)
+top_fr = tk.Frame(dynamic_frame, bg="#f0f0f0")
+top_fr.pack(fill="x", pady=2)
+add_button = tk.Button(top_fr, text="Add", command=add_entry_and_button, bg="#008CBA", fg="white", relief="solid", padx=10, pady=5)
 add_button.pack(pady=5, anchor="e")
+
 
 # make the compiler frame
 code_frame = tk.Frame(root, bg="#dcdcdc", bd=2, relief="solid")
