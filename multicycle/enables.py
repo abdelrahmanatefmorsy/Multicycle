629016709eaee655843ccacc0cafed8d2deb7de7
abdelrahman atef morsy <abdelrahman.atef.morsy@gmail.com>
2024-12-30 14:04:14 +0200

    add new feauters

--- multicycle/enables.py
+++ multicycle/enables.py
@@ -1,3 +1,4 @@
+#!/usr/bin/python3
 from classes import *
 from globalVar import *
 def Multi_cycle(canvas,window,actives):
--- multicycle/main.py
+++ multicycle/main.py
@@ -1,3 +1,4 @@
+#!/usr/bin/python3
 import tkinter as tk
 from classes import *
 from code_check import *
